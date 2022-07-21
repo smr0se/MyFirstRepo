d14eea54c419e0c7b31847d4af2f68db773f3202
SM Rose <smrose@yahoo.com>
2022-07-21 09:00:33 -0700

    Added dev file

--- MyFirstGitCode.ps1
+++ MyFirstGitCode.ps1
@@ -1,6 +1,11 @@
-﻿for ($i = 1; $i -lt 4; $i++)
+﻿iex "git checkout -b dev2"
+
+for ($i = 1; $i -lt 4; $i++)
 {
-New-Item "file$i.txt"
+New-Item "dev_2_$i.txt"
 iex  "git add ."
-iex  "git commit -m 'Added file'"
+iex  "git commit -m 'Added dev file'"
 }
+
+iex "git checkout master"
+iex "git merge dev2 --no-ff"
